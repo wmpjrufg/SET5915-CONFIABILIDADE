202b81f1c18598c0d3f4cfa0b8920c0b1e9bc82d
wmpjrufg <55900120+wmpjrufg@users.noreply.github.com>
2020-11-07 16:56:07 -0300

    novos arquivos

---
+++ FORM_Python/FORM_fmincon.py
@@ -0,0 +1,70 @@
+import numpy as np
+import scipy as sp
+from ERANataf import ERANataf
+from ERADist import ERADist
+"""
+---------------------------------------------------------------------------
+Optimization using the fmincon function (scipy.optimize.minimize() in Python)
+---------------------------------------------------------------------------
+Created by: 
+Matthias Willer (matthias.willer@tum.de)    
+Felipe Uribe (felipe.uribe@tum.de)
+Engineering Risk Analysis Group   
+Technische Universitat Munchen
+www.era.bgu.tum.de
+---------------------------------------------------------------------------
+Version 2018-05
+---------------------------------------------------------------------------
+Comment:
+* The FORM method uses a first order approximation of the LSF and is 
+  therefore not accurate for non-linear LSF's
+---------------------------------------------------------------------------
+Input:
+* g     : limit state function
+* dg    : gradient of the limit state function
+* distr : ERANataf-Object containing the distribution
+---------------------------------------------------------------------------
+Output:
+* u_star : design point in the standard space
+* x_star : design point in the original space
+* beta   : reliability index
+* Pf     : probability of failure
+---------------------------------------------------------------------------
+"""
+
+def FORM_fmincon(g, distr):    
+    # %% initial check if there exists a Nataf object
+    if not(isinstance(distr, ERANataf)): 
+        raise RuntimeError('Incorrect distribution. Please create an ERANataf object!')
+
+    d = len(distr.Marginals)
+
+    # %% objective function
+    dist_fun = lambda u: np.linalg.norm(u)
+
+    # %% parameters of the minimize function
+    u0  = np.tile([0.1],[d,1])  # initial search point
+
+    # nonlinear constraint: H(u) <= 0
+    H    = lambda u: g(distr.U2X(u.reshape(-1,1)))
+    cons = ({'type': 'ineq', 'fun': lambda u: -H(u)})
+
+    # method for minimization
+    alg = 'SLSQP'
+    
+    # %% use constraint minimization
+    res = sp.optimize.minimize(dist_fun, u0, constraints=cons, method=alg)
+
+    # unpack results
+    u_star = res.x
+    beta   = res.fun
+    it     = res.nit
+    
+    # compute design point in orignal space and failure probability
+    x_star = distr.U2X(u_star.reshape(-1,1))
+    Pf     = sp.stats.norm.cdf(-beta)
+    # print results
+    print('*scipy.optimize.minimize() with ', alg, ' Method\n')
+    print(' ', it, ' iterations... Reliability index = ', beta, ' --- Failure probability = ', Pf, '\n\n')
+
+    return u_star, x_star, beta, Pf
---
+++ FORM_Python/main_example_1.py
@@ -0,0 +1,85 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pylab as plt
+from ERANataf import ERANataf
+from ERADist import ERADist
+from FORM_HLRF import FORM_HLRF
+from FORM_fmincon import FORM_fmincon
+plt.close('all')
+"""
+---------------------------------------------------------------------------
+FORM using HLRF algorithm and fmincon: Ex. 1 Ref. 3 - linear function of independent standard normal
+---------------------------------------------------------------------------
+Created by:
+Matthias Willer (matthias.willer@tum.de)    
+Felipe Uribe (felipe.uribe@tum.de)
+Engineering Risk Analysis Group
+Technische Universitat Munchen
+www.era.bgu.tum.de
+---------------------------------------------------------------------------
+Version 2018-05
+---------------------------------------------------------------------------
+Based on:
+1."Structural reliability under combined random load sequences."
+   Rackwitz, R., and B. Fiessler (1979).    
+   Computers and Structures, 9.5, pp 489-494
+2."Lecture Notes in Structural Reliability"
+   Straub (2016)
+---------------------------------------------------------------------------
+"""
+
+# %% definition of the random variables
+d      = 2          # number of dimensions
+pi_pdf = list()
+for i in range(d):
+    pi_pdf.append(ERADist('standardnormal', 'PAR', np.nan)) # n independent rv
+
+# correlation matrix
+R = np.eye(d)   # independent case
+
+# object with distribution information
+pi_pdf = ERANataf(pi_pdf, R)    # if you want to include dependence
+
+# %% limit state function and its gradient in the original space
+beta = 3.5
+g    = lambda x: -np.sum(x, axis=0)/np.sqrt(d) + beta
+dg   = lambda x: np.tile(-1/np.sqrt(d),[d,1])
+
+# %% Solve the optimization problem of the First Order Reliability Method
+
+# OPC 1. FORM using Hasofer-Lind-Rackwitz-Fiessler algorithm HLRF (Ref.1 Pag.128)
+[u_star_hlrf, x_star_hlrf, beta_hlrf, Pf_hlrf] = FORM_HLRF(g, dg, pi_pdf)
+
+# OPC 2. FORM using Python scipy.optimize.minimize()
+[u_star_fmc, x_star_fmc, beta_fmc, Pf_fmc] = FORM_fmincon(g, pi_pdf)
+
+# exact solution
+pf_ex = sp.stats.norm.cdf(-beta)
+
+# show p_f results
+print('***Exact Pf: ', pf_ex, ' ***')
+print('***FORM HLRF Pf: ', Pf_hlrf, ' ***')
+print('***FORM fmincon Pf: ', Pf_fmc, ' ***\n')
+
+# %% Plot HLRF results
+if d == 2:
+    # grid points
+    xx      = np.linspace(0,5,100)
+    [X1,X2] = np.meshgrid(xx,xx)
+    xnod    = np.array([X1,X2])
+    ZX      = g(xnod)
+
+    # figure
+    plt.figure()
+    #
+    plt.pcolor(X1,X2,ZX)
+    plt.contour(X1,X2,ZX,[0]) 
+    plt.plot(0,0,'k.')                                               # origin in standard    
+    plt.plot([0, x_star_hlrf[0]],[0, x_star_hlrf[1]],label='HLRF')   # reliability index beta
+    plt.plot(x_star_hlrf[0],x_star_hlrf[1],'r*')                     # design point in standard
+    plt.plot([0, u_star_fmc[0]],[0, u_star_fmc[1]],label='SLSQP')    # reliability index beta
+    plt.plot(u_star_fmc[0],u_star_fmc[1],'bo')                       # design point in standard    
+    plt.title('Standard space')    
+    plt.legend() 
+    plt.axes().set_aspect('equal', 'box')
+# %%END
